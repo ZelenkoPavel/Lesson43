4be51370c9157fa58ec70e2c4d2170c05176d9f0
ITSTEP\st <st>
2024-02-22 17:26:39 +0300

    test06 completed

--- Project01/test05.cpp
+++ Project01/test05.cpp
@@ -1,24 +1,24 @@
-#include <iostream>
-
-using namespace std;
-
-void swao(int* a, int* b) {
-	int t = *a;
-	*a = *b;
-	*b = t;
-}
-
-int main() {
-
-	int x = 24;
-	int y = 34;
-
-
-	cout << "Before x = " << x << " y = " << y << endl;
-
-	swao(&x, &y);
-
-	cout << "After x = " << x << " y = " << y << endl;
-
-	return 0;
-}
+//#include <iostream>
+//
+//using namespace std;
+//
+//void swao(int* a, int* b) {
+//	int t = *a;
+//	*a = *b;
+//	*b = t;
+//}
+//
+//int main() {
+//
+//	int x = 24;
+//	int y = 34;
+//
+//
+//	cout << "Before x = " << x << " y = " << y << endl;
+//
+//	swao(&x, &y);
+//
+//	cout << "After x = " << x << " y = " << y << endl;
+//
+//	return 0;
+//}
